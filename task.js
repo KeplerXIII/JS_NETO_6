7f91b4d94d8da7869dc0389b90430362c4704333
a.pushkin <a.v.pushkin@gmail.com>
2023-05-19 13:28:56 +0300

    update task_2

--- task.js
+++ task.js
@@ -6,8 +6,26 @@ menuItems.forEach(item => {
 
   if (subMenu) {
     subLink.setAttribute('onclick', 'return false');
-    item.addEventListener('click', () => {
-      subMenu.classList.toggle('menu_active');
+    subLink.addEventListener('click', () => {
+
+        // Закрывает открытое подменю по другой ссылке
+        const activeMenu = document.querySelector('.menu_active')
+        if (activeMenu){
+            if (activeMenu.className !== subMenu.className) {
+                activeMenu.classList.remove('menu_active')
+            }
+        }
+
+        // переключаем видимость подменю
+        subMenu.classList.toggle('menu_active')
+
+        // закрываем подменю по нажатии на ссылку подменю
+        const subMenuLinks = subMenu.querySelectorAll('.menu__link')
+        subMenuLinks.forEach(element => {
+            element.addEventListener('click', () => {
+                subMenu.classList.toggle('menu_active')
+            })
+        });
     });
   }
 });
